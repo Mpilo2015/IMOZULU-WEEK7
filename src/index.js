75ec099013d860758288306ebfc1086212768f59
Mpilo2015 <162923117+Mpilo2015@users.noreply.github.com>
2024-03-25 23:59:38 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -62,7 +62,7 @@ function formatDay(timestamp) {
 }
 
 function getForecast(city) {
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "60tcf1ea0e827fa960dbc14o2834ab1f";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
   axios(apiUrl).then(displayForecast);
 }
@@ -99,4 +99,4 @@ function displayForecast(response) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-searchCity("Paris");
+searchCity("Johannesburg");
